3ebd13f063847c61a637d0f660253aeef6245f80
assermoussa <asser.y.moussa@gmail.com>
2024-01-20 19:41:27 -0600

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = 0;
+
+function upperCaseCustomerName(){
+    customerName = 'BOB'}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 1;
+}
